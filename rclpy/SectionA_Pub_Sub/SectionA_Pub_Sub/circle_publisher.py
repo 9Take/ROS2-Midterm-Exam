6ed3401ef94c3224405d51010fa58e0fbd44731b
Thanpisit Banyam <porsohani@gmail.com>
2025-09-03 18:36:50 +0700

    change code circle_publisher.py

--- rclpy/SectionA_Pub_Sub/SectionA_Pub_Sub/circle_publisher.py
+++ rclpy/SectionA_Pub_Sub/SectionA_Pub_Sub/circle_publisher.py
@@ -6,9 +6,9 @@ class CirclePublisher(Node):
     def __init__(self):
         super().__init__('circle_publisher')
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        
+        self.cnt = 0
         # Timer to publish velocity every 0.1 seconds
-        self.timer = self.create_timer(0.1, self.timer_callback)
+        self.timer = self.create_timer(0.5, self.timer_callback)
         
         # Linear velocity and angular velocity for a circle (radius 0.5 meters)
         self.linear_speed = 0.2  # meters per second
@@ -17,13 +17,19 @@ class CirclePublisher(Node):
         self.get_logger().info(f"Publishing velocity: Linear Speed = {self.linear_speed} m/s, Angular Speed = {self.angular_speed} rad/s")
 
     def timer_callback(self):
+        self.cnt += 1
         msg = Twist()
-        
-        # Set linear velocity (forward speed)
-        msg.linear.x = self.linear_speed
-        
-        # Set angular velocity (turning speed)
-        msg.angular.z = self.angular_speed
+        if (self.cnt >= 10):
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+            self.destroy_timer(self.timer)
+        else:
+            # Set linear velocity (forward speed)
+            msg.linear.x = self.linear_speed
+            
+            # Set angular velocity (turning speed)
+            msg.angular.z = self.angular_speed
+
         
         # Publish the message
         self.publisher_.publish(msg)
